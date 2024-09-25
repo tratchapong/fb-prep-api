92b644867e41493f0b88797968200e14589a4fca
tratchapong <tratchapong@gmail.com>
2024-09-25 17:54:13 +0700

    seed : add profileImage example

--- prisma/seed.js
+++ prisma/seed.js
@@ -7,7 +7,9 @@ const hashedPassword = bcrypt.hashSync('123456', 10)
 const userData = [
     { firstName: 'Andy',lastName:'Codecamp',  password: hashedPassword, email: 'andy@ggg.mail'},
     { firstName: 'Bobby',lastName:'Codecamp', password: hashedPassword, email: 'bobby@ggg.mail'},
-    { firstName: 'Candy',lastName:'Codecamp', password: hashedPassword, mobile: '1111111111'},
+    { firstName: 'Candy',lastName:'Codecamp', password: hashedPassword, mobile: '1111111111',
+        profileImage : 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
+    },
     { firstName: 'Danny',lastName:'Codecamp', password: hashedPassword, mobile: '2222222222'}
 ]
 

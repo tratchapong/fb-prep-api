3e878dcac310099a2d273e45411ff1db2b15f19e
tratchapong <tratchapong@gmail.com>
2024-09-28 17:37:33 +0700

    add profileImage in seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,12 +5,18 @@ const prisma = new PrismaClient()
 const hashedPassword = bcrypt.hashSync('123456', 10)
 
 const userData = [
-    { firstName: 'Andy',lastName:'Codecamp',  password: hashedPassword, email: 'andy@ggg.mail'},
-    { firstName: 'Bobby',lastName:'Codecamp', password: hashedPassword, email: 'bobby@ggg.mail'},
+    { firstName: 'Andy',lastName:'Codecamp',  password: hashedPassword, email: 'andy@ggg.mail',
+        profileImage : 'https://www.svgrepo.com/show/446486/avatar.svg'
+    },
+    { firstName: 'Bobby',lastName:'Codecamp', password: hashedPassword, email: 'bobby@ggg.mail',
+        profileImage : 'https://www.svgrepo.com/show/407389/scientist-medium-dark-skin-tone.svg'
+    },
     { firstName: 'Candy',lastName:'Codecamp', password: hashedPassword, mobile: '1111111111',
         profileImage : 'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
     },
-    { firstName: 'Danny',lastName:'Codecamp', password: hashedPassword, mobile: '2222222222'}
+    { firstName: 'Danny',lastName:'Codecamp', password: hashedPassword, mobile: '2222222222',
+        profileImage : 'https://www.svgrepo.com/show/404551/avatar-man-profile-user-5.svg'
+    }
 ]
 
 console.log('Seed...')
